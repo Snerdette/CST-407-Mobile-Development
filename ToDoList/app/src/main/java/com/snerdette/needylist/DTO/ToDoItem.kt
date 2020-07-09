34f1252e34358d6a58bc467d494bc65bf474315a
Kate LaFrance <Kate.LaFrance@oit.edu>
2020-07-08 22:33:01 -0700

    More progress

---
+++ ToDoList/app/src/main/java/com/snerdette/needylist/DTO/ToDoItem.kt
@@ -0,0 +1,8 @@
+package com.snerdette.needylist.DTO
+
+class ToDoItem () {
+    var id : Long = -1
+    var toDoId : Long = -1
+    var itemName = ""
+    var isComplete = false
+}
--- ToDoList/app/src/main/java/com/snerdette/needylist/DashboardActivity.kt
+++ ToDoList/app/src/main/java/com/snerdette/needylist/DashboardActivity.kt
@@ -1,11 +1,37 @@
 package com.snerdette.needylist
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.snerdette.needylist.DTO.ToDo
+import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class DashboardActivity : AppCompatActivity() {
+
+    lateinit var dbHandler : DBHandler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        dbHandler = DBHandler(this)
+        fab_dashboard.setOnClickListener {
+            val dialog = AlertDialog.Builder(this)
+            val view = layoutInflater.inflate(R.layout.dialog_dashboard, null)
+            val toDoName = view.findViewById<EditText>(R.id.ev_todo)
+            dialog.setPositiveButton("Add") { _: DialogInterface, _: Int ->
+               if(toDoName.text.isNotEmpty()){
+                   val toDo = ToDo()
+                   toDo.name = toDoName.text.toString()
+                   dbHandler.addToDo(toDo)
+               }
+            }
+            dialog.setNegativeButton("Cancel") {
+                _: DialogInterface,
+                _: Int -> 
+            }
+            dialog.show()
+        }
     }
 }
