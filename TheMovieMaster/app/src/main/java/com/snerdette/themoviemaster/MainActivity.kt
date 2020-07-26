d1e517e033d9202c5a942f6db05eead799cb9751
Kate LaFrance <Kate.LaFrance@oit.edu>
2020-07-25 18:58:22 -0700

    Movie list is now loading and scrollable! next is onto pagination

--- TheMovieMaster/app/src/main/java/com/snerdette/themoviemaster/MainActivity.kt
+++ TheMovieMaster/app/src/main/java/com/snerdette/themoviemaster/MainActivity.kt
@@ -5,14 +5,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 //import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var popularMovies: RecyclerView
+    private lateinit var popularMoviesAdapter: MoviesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        popularMovies = findViewById(R.id.popular_movies)
+        popularMovies.layoutManager = LinearLayoutManager(
+                this,
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        popularMoviesAdapter = MoviesAdapter(listOf())
+        popularMovies.adapter = popularMoviesAdapter
 
         MoviesRepository.getPopularMovies(
             onSuccess = ::onPopularMoviesFetched,
@@ -21,7 +35,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onPopularMoviesFetched(movies: List<Movie>) {
-        Log.d("MainActivity", "Movies: $movies")
+        popularMoviesAdapter.updateMovies(movies)
     }
 
     private fun onError() {
