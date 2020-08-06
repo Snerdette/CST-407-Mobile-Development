0eda0b82659c07db2d62dafffcc77262f51a2900
Kate LaFrance <Kate.LaFrance@oit.edu>
2020-08-06 16:51:36 -0700

    adding in assignment 4

--- TheMovieMaster/app/src/main/java/com/snerdette/themoviemaster/Api.kt
+++ TheMovieMaster/app/src/main/java/com/snerdette/themoviemaster/Api.kt
@@ -13,12 +13,12 @@ interface Api {
 
     @GET("movie/top_rated")
     fun getTopRatedMovies(
-        @Query("api_key") apiKey: String = "YOUR_API_KEY_HERE",
+        @Query("api_key") apiKey: String = "e4b524f368b538bcc3df7496161f39a9",
         @Query("page") page: Int
     ): Call<GetMoviesResponse>
 
     @GET("movie/upcoming") fun getUpcomingMovies(
-        @Query("api_key") apiKey: String = "YOUR_API_KEY_HERE",
+        @Query("api_key") apiKey: String = "e4b524f368b538bcc3df7496161f39a9",
         @Query("page") page: Int
     ): Call<GetMoviesResponse>
 }
